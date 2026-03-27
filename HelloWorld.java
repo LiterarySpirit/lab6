65025eaa7b55bdb3a4049e06adaab36de504dcbe
Mert Ertürk <merterturk@Merts-MacBook-Pro.local>
2026-03-27 12:15:38 +0000

    add HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String message() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
